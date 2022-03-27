aff5bf1804ae62fb9f193b3172853247b586090b
sudomakecoffee <brisk.home1381@sudomakecoffee.io>
2022-03-27 18:03:03 -0400

    Setup initial view for displaying text in center of screen

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,53 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="wrapper">
+    <h1>Hello</h1>
+  </div>
 </template>
 
 <style>
-@import "./assets/base.css";
+@import "@/assets/base.css";
 
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
+  background-color: #77f6fd;
+
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: center;
 
   font-weight: normal;
 }
 
-header {
-  line-height: 1.5;
+.wrapper {
+  display: flex;
+  justify-content: center;
+
+  max-width: 50%;
+  width: 100%;
+  max-height: 50%;
+
+  background-color: rgba(24, 24, 24, 0.33);
+  color: white;
+
+  padding: 8rem 4rem;
+  border-radius: 0.5rem;
+
+  /* filter: opacity(0.5); */
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
+.wrapper h1 {
+  display: inline;
 }
 
 @media (min-width: 1024px) {
   body {
     display: flex;
     place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 }
 </style>
